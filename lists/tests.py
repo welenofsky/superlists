f33cf13cd61f6e132e56564fef42c2b0ae5d14ae
Justin Welenofsky <justin@welenofsky.com>
2014-08-25 13:40:43 -0700

    First unit test and urld mapping, dummy view

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,8 @@
+from django.core.urlresolvers import resolve
 from django.test import TestCase
-# Here lies all my unit tests for the lists app
-# Create your tests here.
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-	def test_bad_maths(self):
-		self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+	def test_root_url_resolves_to_home_page_view(self):
+		found = resolve('/')
+		self.assertEqual(found.func, home_page)
--- superlists/urls.py
+++ superlists/urls.py
@@ -6,5 +6,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    # Currently not using the admin page
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'lists.views.home_page', name='home'),
 )
