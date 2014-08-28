8ea46c81a6284f01273f35771921c68600f2fbc8
Justin Welenofsky <justin@welenofsky.com>
2014-08-28 15:11:42 -0700

    Added functional_tests to ensure that a unique URL is returned, some refactoring

--- superlists/urls.py
+++ superlists/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # Currently not using the admin page
     # url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list', name='view_list'),
 )
