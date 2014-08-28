f4d5ce4c1c02d05269231a62abf5f2a36be9cba5
Justin Welenofsky <justin@welenofsky.com>
2014-08-28 13:41:48 -0700

    make functional tests and app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,12 +4,15 @@
 # Description: Functional Tests, also called                        #
 #   black box, end-to-end or acceptance Tests                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from django.test  import LiveServerTestCase
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+
 import unittest
 import time
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
@@ -25,7 +28,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_start_a_list_and_retrieve_it(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
-		self.browser.get('http://localhost:8000')
+		self.browser.get(self.live_server_url)
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
@@ -53,7 +56,7 @@ class NewVisitorTest(unittest.TestCase):
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.RETURN)
-		
+
 		# The page updates again, and now shows both items on her list
 		self.check_for_row_in_list_table('1: Buy peacock feathers')
 		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -64,6 +67,3 @@ class NewVisitorTest(unittest.TestCase):
 		# She visits that URL - her to-do list is still there.
 
 		# Satisfied, she goes back to sleep
-
-if __name__ == '__main__':
-	unittest.main(warnings='ignore')
