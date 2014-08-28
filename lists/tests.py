cedf253408d0a691b277e3ef6cd198fd2c6484f6
Justin Welenofsky <justin@welenofsky.com>
2014-08-28 09:30:05 -0700

    added a item model and refactored render in home_page()

--- lists/tests.py
+++ lists/tests.py
@@ -25,7 +25,6 @@ class ItemModelTest(TestCase):
 		self.assertEqual(second_saved_item.text, 'Item the second')
 
 class HomePageTest(TestCase):
-
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -42,6 +41,11 @@ class HomePageTest(TestCase):
 		request.POST['item_text'] = 'A new list item'
 
 		response = home_page(request)
+		
+		self.assertEqual(Item.objects.count(), 1)
+		new_item = Item.objects.first()
+		self.assertEqual(new_item.text, 'A new list item')
+
 		self.assertIn('A new list item', response.content.decode())
 		expected_html = render_to_string(
 			'home.html',
