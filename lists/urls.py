dc8b0b7e30c0f57ddd2694e5ce69645a11f43143
Justin Welenofsky <justin@welenofsky.com>
2014-09-02 12:22:22 -0700

    Refactored: moved lists specific urls from superlists/urls.py lists/urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
 
     # Currently not using the admin page
     # url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'lists.views.home_page', name='home'),
-    url(r'^lists/', include('lists.urls'))
+    url(r'^(\d+)/$', 'lists.views.view_list', name='view_list'),
+    url(r'^(\d+)/add_item$', 'lists.views.add_item', name='add_item'),
+    url(r'^new$', 'lists.views.new_list', name='new_list'),
 )
