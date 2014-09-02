b5667e82a010cc4a462354d35a7258173d6435d4
Justin Welenofsky <justin@welenofsky.com>
2014-09-02 11:16:07 -0700

    Added list model and required List item to have parent item

--- lists/tests.py
+++ lists/tests.py
@@ -3,32 +3,43 @@ from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 class ListViewTest(TestCase):
+
 	def test_uses_list_template(self):
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 		self.assertTemplateUsed(response, 'list.html')
 
 	def test_displays_all_items(self):
-		Item.objects.create(text='itemey 1')
-		Item.objects.create(text='itemey 2')
+		list_ = List.objects.create()
+		Item.objects.create(text='itemey 1', list=list_)
+		Item.objects.create(text='itemey 2', list=list_)
 
 		response = self.client.get('/lists/the-only-list-in-the-world/')
 
 		self.assertContains(response, 'itemey 1')
 		self.assertContains(response, 'itemey 2')
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
+
 	def test_saving_and_retrieving_items(self):
+		list_ = List()
+		list_.save()
+
 		first_item = Item()
 		first_item.text = 'The first (ever) list item'
+		first_item.list = list_
 		first_item.save()
 
 		second_item = Item()
 		second_item.text = 'Item the second'
+		second_item.list = list_
 		second_item.save()
+
+		saved_list = List.objects.first()
+		self.assertEqual(saved_list, list_)
 
 		saved_items = Item.objects.all()
 		self.assertEqual(saved_items.count(), 2)
@@ -36,9 +47,12 @@ class ItemModelTest(TestCase):
 		first_saved_item = saved_items[0]
 		second_saved_item = saved_items[1]
 		self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+		self.assertEqual(first_saved_item.list, list_)
 		self.assertEqual(second_saved_item.text, 'Item the second')
+		self.assertEqual(second_saved_item.list, list_)
 
 class HomePageTest(TestCase):
+
 	def test_root_url_resolves_to_home_page_view(self):
 		found = resolve('/')
 		self.assertEqual(found.func, home_page)
@@ -50,6 +64,7 @@ class HomePageTest(TestCase):
 		self.assertEqual(response.content.decode(), expected_html)
 
 class NewListTest(TestCase):
+
 	def test_saving_a_POST_request(self):
 		self.client.post(
 			'/lists/new',
