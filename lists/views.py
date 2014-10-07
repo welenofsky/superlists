38d3785f1999b177b3088f0b7c5209c09c6ede51
Justin Welenofsky <justin@welenofsky.com>
2014-10-07 12:26:19 -0700

    Added bootstrap

--- lists/views.py
+++ lists/views.py
@@ -1,25 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
 from django.shortcuts import redirect, render
 
 from lists.models import Item, List
 
-# Create your views here.
+
 def home_page(request):
     return render(request, 'home.html')
 
+
 def view_list(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	items = Item.objects.filter(list=list_)
-	return render(request, 'list.html', {'list': list_})
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
+
 
 def new_list(request):
-	list_ = List.objects.create()
+    list_ = List.objects.create()
 
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/%d/' % (list_.id,))
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
+
 
 def add_item(request, list_id):
-	list_ = List.objects.get(id=list_id)
-	Item.objects.create(text=request.POST['item_text'], list=list_)
-	return redirect('/lists/%d/' % (list_.id,))
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/%d/' % (list_.id,))
